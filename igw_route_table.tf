3d5aa97780803068cf16f9f9ae36ed760b19984c
Kiran Madabhushi <maskiran@gmail.com>
2022-09-30 07:50:30 -0700

    use just one route table for igw

--- igw_route_table.tf
+++ igw_route_table.tf
@@ -2,7 +2,6 @@ resource "aws_route_table" "igw" {
   depends_on = [
     valtix_gateway.fwd_gw
   ]
-  for_each = var.zones
   vpc_id   = aws_vpc.vpc.id
   tags = {
     Name   = "${var.prefix}-igw-${each.key}"
@@ -11,14 +10,13 @@ resource "aws_route_table" "igw" {
 }
 
 resource "aws_route_table_association" "igw" {
-  for_each       = var.zones
-  route_table_id = aws_route_table.igw[each.key].id
+  route_table_id = aws_route_table.igw.id
   gateway_id     = aws_internet_gateway.igw.id
 }
 
 resource "aws_route" "to_app" {
   for_each               = var.zones
-  route_table_id         = aws_route_table.igw[each.key].id
+  route_table_id         = aws_route_table.igw.id
   destination_cidr_block = aws_subnet.app[each.key].cidr_block
   vpc_endpoint_id        = local.gwlbe_by_zones[each.key]
 }
